7481ae609c8218f6c6c1f2925c4a5861702d93e6
fahime abouhamze <abouhamze.fahime@gmail.com>
2023-01-13 08:57:31 +0330

    add Identity

---
+++ ProjectUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
