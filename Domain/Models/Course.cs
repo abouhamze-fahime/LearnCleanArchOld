9f0489bfe9a61b78c90566bc622be4ed3f0fc0b8
fahime abouhamze <abouhamze.fahime@gmail.com>
2023-01-13 09:08:06 +0330

    add course model

---
+++ Domain/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+   public class Course
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+
+    }
+}
